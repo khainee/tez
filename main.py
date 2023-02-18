82d12efc428d2836e7f1de4c6a6f2b7475f09231
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-02-18 09:33:12 +0630

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ import requests
 #     "bot",
 #     api_id=api_id,
 #     api_hash=api_hash,
-#     bot_token=bot_token)
+#     bot_token=5865175027:AAHLrgozMsIxvXKv8TSDG1yAeL1RMw5rmIE)
 
 bot = Client(
     "bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
+    bot_token=os.environ.get("BOT_TOKEN", "5865175027:AAHLrgozMsIxvXKv8TSDG1yAeL1RMw5rmIE"),
+    api_id=int(os.environ.get("API_ID", 18748626)),
+    api_hash=os.environ.get("API_HASH", "3c0ef7e51ef52305a17179019575ccc9")
 )
 
 logger = logging.getLogger()
